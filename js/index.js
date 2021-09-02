035ffaf6ed7ba5de401e4c552c6329125c33e340
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-09-02 15:20:09 +0600

    same bug fix

--- js/index.js
+++ js/index.js
@@ -51,8 +51,8 @@ const displaySearchResult = books => {
                 <img src="https://covers.openlibrary.org/b/id/${book.cover_i }-M.jpg" class="fluid w-50 h-75 mx-auto mt-2" alt="...">
                 <div class="card-body">
                   <a href=""><h4 class="card-title">${book.title}</h4></a> 
-                  <p class="card-text"> <span  class="author_name"><i>Author: ${book.author_name ?book.author_name:"N/a"}</i></span></p>
-                  <p class="card-text"> <span  class="">Publisher: ${book.publisher}</span></p
+                  <p class="card-text"> <span  class="author_name"><i>Author:</span> ${book.author_name ?book.author_name:"N/a"}</i></p>
+                  <p class="card-text"> <span  class="publisher">Publisher:</span> ${book.publisher}</p
                 </div>
                   <div class="card-footer">
                   <small class="text-muted"><p class="card-text"><span class="publish_year">First published ${book.first_publish_year}</span></p></small>
